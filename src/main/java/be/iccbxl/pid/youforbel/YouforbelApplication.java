b381a0fe01aaad1d4d623028761447ea098f4ab5
TonNomGitHub <ton.email@exemple.com>
2026-01-30 14:35:43 +0100

    Initial Spring Boot project

---
+++ src/main/java/be/iccbxl/pid/youforbel/YouforbelApplication.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.youforbel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouforbelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouforbelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/iccbxl/pid/youforbel/YouforbelApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.youforbel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouforbelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
