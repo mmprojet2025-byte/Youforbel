39eefee705466d59d5ec72df2541be4954315245
Mardoche Malaba <mmprojet2025@gmail.com>
2026-04-30 15:08:52 +0200

    Connect Spring Security to users database

--- src/main/java/be/iccbxl/pid/youforbel/config/SecurityConfig.java
+++ src/main/java/be/iccbxl/pid/youforbel/config/SecurityConfig.java
@@ -2,23 +2,37 @@ package be.iccbxl.pid.youforbel.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import be.iccbxl.pid.youforbel.model.User;
+import be.iccbxl.pid.youforbel.repository.UserRepository;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        UserDetails user = User
-                .withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin123")
-                .roles("ADMIN")
-                .build();
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository repository) {
 
-        return new InMemoryUserDetailsManager(user);
+        return username -> {
+            User user = repository.findByLogin(username);
+
+            if (user == null) {
+                throw new UsernameNotFoundException("User not found");
+            }
+
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(user.getLogin())
+                    .password(user.getPassword())
+                    .roles(user.getRole().name())
+                    .build();
+        };
     }
 }
