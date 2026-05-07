9de23825608e85b4572c9ea5750071436c697fdb
Mardoche Malaba <mmprojet2025@gmail.com>
2026-05-07 10:51:06 +0200

    Ajout de la page d’accueil et amélioration de la navigation

--- src/main/java/be/iccbxl/pid/youforbel/controller/HomeController.java
+++ src/main/java/be/iccbxl/pid/youforbel/controller/HomeController.java
@@ -1,13 +1,17 @@
 package be.iccbxl.pid.youforbel.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String index(Model model) {
+
+        model.addAttribute("title", "Accueil");
+
+        return "home";
     }
 }
